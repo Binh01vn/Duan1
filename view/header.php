371e39456cf581a608fe35d61336f8f5c655e406
linkbaodoi <linhnthph37801@fpt.edu.vn>
2023-12-05 17:13:28 +0700

    cap nhat chieu 5-12

--- view/header.php
+++ view/header.php
@@ -6,8 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GIÀY NIKE BAK</title>
-    <!-- <meta name="robots" content="noindex, follow" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="view/assets/images/favicon.png">
 
