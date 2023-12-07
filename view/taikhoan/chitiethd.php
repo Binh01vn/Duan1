44bb05b34f539a9a2616f0016f4e1c9f50c38322
khanhprox <khanhvdph37690@fpt.edu.vn>
2023-12-07 17:34:32 +0700

    cap nhat full chieu 7-12

--- view/taikhoan/chitiethd.php
+++ view/taikhoan/chitiethd.php
@@ -1,3 +1,10 @@
+<?php
+if(!empty($_SESSION['username'])){
+    extract($_SESSION['username']);
+}else{
+    header('Location: ?act=sigorreg');
+}
+?>
 <!-- Begin Kenne's Breadcrumb Area -->
 <div class="breadcrumb-area">
     <div class="container">
@@ -16,6 +23,17 @@
 <div class="kenne-login-register_area">
     <div class="container">
         <div class="row">
+            <div class="ttcanhan">
+                <div class="titleh3">
+                    <h3>Thông tin người nhận</h3>
+                </div>
+                <div>
+                    <p><u>Người nhận hàng: </u><b><?= $tensohuu ?></b></p>
+                    <p><u>Số điện thoại: </u><b><?= $phone ?></b></p>
+                    <p><u>Email: </u><b><?= $email ?></b></p>
+                    <p><u>Nơi nhận hàng: </u><b><?= $diachi ?></b></p>
+                </div>
+            </div>
             <div class="titleh3">
                 <h3>Chi tiết hóa đơn</h3>
             </div>
@@ -32,10 +50,10 @@
                 <tbody>
                     <?php
                     $listhd = select_hoadon();
-                    foreach($listhd as $lhd) {
+                    foreach ($listhd as $lhd) {
                         extract($lhd);
                         $idfhd = $id_hd;
-                        if($iduser == $_SESSION['username']['idacc'] && $idfhd == $_GET['idhd']) {
+                        if ($iduser == $_SESSION['username']['idacc'] && $idfhd == $_GET['idhd']) {
                             $tthoadon = $tonghd;
                             $trangthaihd = $trangthai;
                             $trangthai_tt = $trangthaitt;
@@ -49,36 +67,36 @@
                                 </td>
                                 <td>
                                     <?php
-                                    if($pttt == 1) {
+                                    if ($pttt == 1) {
                                         echo "Thanh toán trực tiếp";
-                                    } else if($pttt == 2) {
+                                    } else if ($pttt == 2) {
                                         echo "Thanh toán MOMO";
                                     }
                                     ?>
                                 </td>
                                 <td>
                                     <?php
-                                    if($trangthaihd == 0) {
+                                    if ($trangthai == 0) {
                                         echo "Chờ xác nhận.";
-                                    } else if($trangthaihd == 1) {
+                                    } else if ($trangthai == 1) {
                                         echo "Đã xác nhận.";
-                                    } else if($trangthaihd == 2) {
+                                    } else if ($trangthai == 2) {
                                         echo "Đang chuẩn bị hàng.";
-                                    } else if($trangthaihd == 3) {
+                                    } else if ($trangthai == 3) {
                                         echo "Đang giao hàng.";
-                                    } else if($trangthaihd == 4) {
-                                        echo "Đã nhận hàng.";
-                                    } else if($trangthaihd == 5) {
-                                        echo "Đơn hàng bị hủy.";
+                                    } else if ($trangthai == 4) {
+                                        echo '<b style="color: green;">Đã nhận hàng.</b>';
+                                    } else if ($trangthai == 5) {
+                                        echo '<div style="color: red;">Đơn hàng bị hủy.</div>';
                                     }
                                     ?>
                                 </td>
                                 <td>
                                     <?php
-                                    if($trangthaitt == 0) {
-                                        echo "Chưa thanh toán.";
-                                    } else if($trangthaitt == 1) {
-                                        echo "Đã thanh toán.";
+                                    if ($trangthaitt == 0) {
+                                        echo '<div style="color: red;">Chưa thanh toán.</div>';
+                                    } else if ($trangthaitt == 1) {
+                                        echo '<b style="color: green;">Đã thanh toán.</b>';
                                     }
                                     ?>
                                 </td>
@@ -101,11 +119,11 @@
                 <tbody>
                     <?php
                     $listbhd = select_billhoadon();
-                    foreach($listbhd as $lbhd) {
+                    foreach ($listbhd as $lbhd) {
                         extract($lbhd);
-                        if($_GET['idhd'] == $idhd) {
+                        if ($_GET['idhd'] == $idhd) {
                             $idfhd = $idhd;
-                            $linksp = "index.php?act=sanphamct&idsp=".$idspcart;
+                            $linksp = "index.php?act=sanphamct&idsp=" . $idspcart;
                             ?>
                             <tr>
                                 <td>
@@ -117,13 +135,13 @@
                                     <?= $sizespcart ?>
                                 </td>
                                 <td>
-                                    <?= number_format((int)$gspcart, 0, ",", ".") ?>
+                                    <?= number_format((int) $gspcart, 0, ",", ".") ?>
                                 </td>
                                 <td>
                                     <?= $soluongspcart ?>
                                 </td>
                                 <td>
-                                    <?= number_format((int)$tongtien, 0, ",", ".") ?>
+                                    <?= number_format((int) $tongtien, 0, ",", ".") ?>
                                 </td>
                             </tr>
                         <?php }
@@ -134,7 +152,7 @@
                             <h6>Tổng tiền thanh toán (VND):</h6>
                         </td>
                         <td>
-                            <?= number_format((int)$tthoadon, 0, ",", ".") ?>
+                            <?= number_format((int) $tthoadon, 0, ",", ".") ?>
                         </td>
                     </tr>
                 </tbody>
@@ -145,32 +163,32 @@
             <div class="col-12">
                 <div class="coupon-all">
                     <?php
-                    if($trangthaihd == 3) {
+                    if ($trangthaihd == 3) {
                         echo '
                         <div class="coupon">
-                            <a href="index.php?act=xacnhandh&trangthai=4&trangthaitt=1&idhd='.$idfhd.'" class="button">Đã nhận được
+                            <a href="index.php?act=xacnhandh&trangthai=4&trangthaitt=1&idhd=' . $idfhd . '" class="button">Đã nhận được
                                 hàng</a>
                         </div>
                         ';
                     } else {
-                        if($trangthai_tt == 1 && $trangthaihd != 5) {
+                        if ($trangthai_tt == 1 && $trangthaihd != 5) {
                             echo
                                 '<div class="coupon">
                                     <a href="index.php?act=sanpham" class="button">Mua sắm thêm</a>
                                 </div>';
-                        } else if($trangthaihd == 5){
+                        } else if ($trangthaihd == 5) {
                             echo
                                 '<div class="coupon">
-                                    <a href="index.php?act=xacnhandh&trangthai=0&idhd='.$idfhd.'" class="button">Đặt hàng lại</a>
+                                    <a href="index.php?act=xacnhandh&trangthai=0&idhd=' . $idfhd . '" class="button">Đặt hàng lại</a>
                                 </div>';
-                        } 
+                        }
                     }
                     ?>
                     <?php
-                    if($trangthaihd == 0 || $trangthaihd == 1 || $trangthaihd == 2) {
+                    if ($trangthaihd == 0 || $trangthaihd == 1 || $trangthaihd == 2) {
                         echo
                             '<div class="coupon2">
-                            <a href="index.php?act=xacnhandh&trangthai=5&idhd='.$idfhd.'" class="button">Hủy đơn hàng</a>
+                            <a href="index.php?act=xacnhandh&trangthai=5&idhd=' . $idfhd . '" class="button">Hủy đơn hàng</a>
                         </div>';
                     }
                     ?>
